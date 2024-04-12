ce56caa3fd3944842cb361a958170ab4a8fd1cfb
dawe <dawedawe@posteo.de>
2024-04-12 11:14:24 +0200

    add a fable project to show the parser/interpreter in the browser

---
+++ src/AfLang.Js/Program.fs
@@ -0,0 +1,22 @@
+﻿open Browser
+open FSharp.Text.Lexing
+open Lang.Interpreter
+
+let parse source =
+    let lexbuf = LexBuffer<char>.FromString source
+
+    try
+        Parser.start Lexer.read lexbuf
+    with _ ->
+        None
+
+let input = document.getElementById "input" :?> Browser.Types.HTMLTextAreaElement
+let output = document.getElementById "output" :?> Browser.Types.HTMLTextAreaElement
+let runButton = document.getElementById "run" :?> Browser.Types.HTMLButtonElement
+runButton.onclick <- fun _ ->
+    let source = input.value
+    let ast = parse source
+    match ast with
+    | Some a ->
+        output.value <- run a |> string
+    | None -> output.value <- "parse error"
---
+++ src/AfLang.Js/README.md
@@ -0,0 +1,15 @@
+# aflang.js
+
+To install dependencies:
+
+```bash
+bun install
+```
+
+To run:
+
+```bash
+dotnet fable watch --outDir out --verbose --run bunx vite
+```
+
+This project was created using `bun init` in bun v1.1.2. [Bun](https://bun.sh) is a fast all-in-one JavaScript runtime.
