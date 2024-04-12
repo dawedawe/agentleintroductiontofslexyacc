de5ba7faf862bd4c26fcf461862455acc46a131a
dawe <dawedawe@posteo.de>
2024-04-12 14:40:45 +0200

    add base to vite.config

--- src/AfLang.Js/vite.config.ts
+++ src/AfLang.Js/vite.config.ts
@@ -4,5 +4,6 @@ import fable from "vite-plugin-fable"
 // https://vitejs.dev/config/
 export default defineConfig({
     clearScreen: false,
-    plugins: [ fable() ]
+    plugins: [ fable() ],
+    base: '/agentleintroductiontofslexyacc/'
 })
