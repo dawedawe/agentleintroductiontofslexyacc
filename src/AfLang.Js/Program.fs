5807aa6977acc33c63d5f056aa5d3dc5ef24bd2c
dawe <dawedawe@posteo.de>
2024-04-12 12:36:03 +0200

    support multi line scripts

--- src/AfLang.Js/Program.fs
+++ src/AfLang.Js/Program.fs
@@ -13,10 +13,14 @@ let parse source =
 let input = document.getElementById "input" :?> Browser.Types.HTMLTextAreaElement
 let output = document.getElementById "output" :?> Browser.Types.HTMLTextAreaElement
 let runButton = document.getElementById "run" :?> Browser.Types.HTMLButtonElement
-runButton.onclick <- fun _ ->
-    let source = input.value
-    let ast = parse source
-    match ast with
-    | Some a ->
-        output.value <- run a |> string
-    | None -> output.value <- "parse error"
+
+runButton.onclick <-
+    fun _ ->
+        let sourceLines = input.value.Split('\r', '\n')
+        let exprs = sourceLines |> Array.map parse
+
+        if Array.contains None exprs then
+            output.value <- "parse error"
+        else
+            let exprs = Array.choose id exprs |> List.ofArray
+            output.value <- execute exprs |> string
